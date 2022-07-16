e7ef3fe801d54b5fb649dc59d845497d1a884f36
tugbaozturk66 <kilicertugba@gmail.com>
2022-07-16 21:23:20 +0200

    Githup

--- src/main/java/MyGitClass.java
+++ src/main/java/MyGitClass.java
@@ -1,6 +1,7 @@
 public class MyGitClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("I am learning Githup!!");
     }
 
 }
