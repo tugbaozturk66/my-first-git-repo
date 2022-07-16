7ce7ca8b8e24a2a2720bc284b17fedcbb73c8bb8
tugbaozturk66 <kilicertugba@gmail.com>
2022-07-16 21:47:11 +0200

    adding one more

--- src/main/java/MyGitClass.java
+++ src/main/java/MyGitClass.java
@@ -2,6 +2,9 @@ public class MyGitClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("I am learning Githup!!");
+        System.out.println("I am adding new code...");
+        System.out.println("I am adding one more");
+
     }
 
 }
